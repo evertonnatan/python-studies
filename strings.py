b797d97c02b8ca8939c77bcfe1d5c26d530b89d5
evertonnatan <evertonnatandl@outlook.com>
2021-09-15 08:28:19 -0300

    Aprofundando um pouco mais os estudos sobre Strings

--- strings.py
+++ strings.py
@@ -4,3 +4,19 @@
 # Escapando caracteres:
 
 print("Esse é meu \"texto\" (str)")
+
+# O "*" operador cria várias cópias de uma string. Exemplo:
+'''
+>>> s = 'foo.'
+
+>>> s * 4
+'foo.foo.foo.foo.'
+>>> 4 * s
+'foo.foo.foo.foo.'
+
+# Caso o operando multiplicador seja um número negativo ou zero, o resultado será um String vazia, como no exemplo abaixo:
+>>> 'foo' * -8
+''
+
+
+'''
