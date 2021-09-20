47290de19a99503672faf179393a955243c90740
evertonnatan <evertonnatandl@outlook.com>
2021-09-20 09:29:17 -0300

    Aula de tipos de dados em Python finalizada

--- tipos-de-dados.py
+++ tipos-de-dados.py
@@ -14,3 +14,27 @@ print(type("Natã"))
 print(type(1564))
 print(type(1.564))
 print(type(True))
+
+# bool:
+print(bool(''))  # False
+print(bool([]))  # False
+print(bool(0))  # False
+
+# string:
+print('10' + '10')
+
+# EXERCÍCIO DA AULA:
+
+# String: nome
+print('Everton Natan', type('Everton Natan'))
+
+# Inteiro: idade
+print(25, type(25))
+
+# Float: altura
+print(1.67, type(1.67))
+
+# Boolean: é maior de idade?
+print(25 > 18, type(25 > 18))
+
+
