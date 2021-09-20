3e76df5b1a3cf0ba9c87cd456807ec31d32cd177
evertonnatan <evertonnatandl@outlook.com>
2021-09-20 09:15:50 -0300

    Aula de tipos de dados em Python

---
+++ tipos-de-dados.py
@@ -0,0 +1,16 @@
+'''
+
+TIPOS DE DADOS:
+
+srt => string
+int => inteiros
+float => real/ponto flutuante
+bool => booleano/lógico
+
+
+'''
+
+print(type("Natã"))
+print(type(1564))
+print(type(1.564))
+print(type(True))
