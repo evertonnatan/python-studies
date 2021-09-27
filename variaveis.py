0a467873fc7774bff401e51c10bd6ddc8fed6857
evertonnatan <evertonnatandl@outlook.com>
2021-09-27 07:59:39 -0300

    Learning about variables in python

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+#  Python permite atribuição encadeada, o que torna possível atribuir o mesmo valor a várias variáveis simultaneamente:
+
+a = b = c = 300
+print(a, b, c)
+
+#  Em Python, uma variável pode receber um valor de um tipo e, posteriormente, reatribuir um valor de um tipo diferente:
+var = 23.5
+print(var)
+var = "Now I'm a string"
+print(var)
+print(id(var)) #  função Python embutida, id()retorna o identificador inteiro de um objeto.
+
