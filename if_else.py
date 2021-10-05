c87bb2482f0604fd4419604dfd32d9261a544736
evertonnatan <evertonnatandl@outlook.com>
2021-10-04 21:17:27 -0300

    Aula de IF, ELIF e ELSE

---
+++ if_else.py
@@ -0,0 +1,12 @@
+'''
+Condições IF, ELIF e ELSE 
+'''
+
+if False:
+    print("Verdadeiro.")
+elif False:
+    print("Agora é verdadeiro")
+elif True:
+    print("Mais uma verdadeira")
+else:
+    print("Não é verdadeiro")
