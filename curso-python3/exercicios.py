49ef79768ce00df02cf06a39a3d4935f273e219c
evertonnatan <evertonnatandl@outlook.com>
2022-02-28 21:50:03 -0300

    Resolvendo o primeiro exercício.

--- curso-python3/exercicios.py
+++ curso-python3/exercicios.py
@@ -3,12 +3,29 @@ Faça um programa que peça ao usuário para digitar um número inteiro, informe
 
 """
 
+from re import A
 
+
+numero_inteiro = input('Digite um número inteiro: ')
+
+if numero_inteiro.isdigit():
+    numero_inteiro = int(numero_inteiro)
+
+    if numero_inteiro % 2 == 0:
+        print(f"{numero_inteiro} é par!")
+    else:
+        print(f"{numero_inteiro} é ímpar!")
+else:
+    print("Isso não é um número inteiro!")
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário descrito, exiba a saudação apropriada. Ex.: Bom dia, Boa tarde, Boa noite!
 
 """
+
+
+
+
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos, escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva "Seu nome é normal"; maior que 6 letras, escreva "Seu nome é muito grande".
