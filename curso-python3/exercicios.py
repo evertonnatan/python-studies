9752e10f75318a0fbee1c4463db2b7c7b76447a7
evertonnatan <evertonnatandl@outlook.com>
2022-02-28 22:00:37 -0300

    Resolvendo o terceiro exercício

--- curso-python3/exercicios.py
+++ curso-python3/exercicios.py
@@ -43,3 +43,12 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 
 """
 
+nome = input("Digite seu nome: ")
+tamanho = len(nome)
+
+if tamanho <= 4:
+    print("Seu nome é curto!")
+elif tamanho <= 6:
+    print("Seu nome é normal")
+else:
+    print("Seu nome é muito grande!")
