2a6f51f1a7c1dccf275eb8b06ec07f9f9f2c28b5
evertonnatan <evertonnatandl@outlook.com>
2022-03-01 20:13:51 -0300

    Aprofundando a formatação de valores

---
+++ curso-python3/formatando_valores.py
@@ -0,0 +1,15 @@
+"""
+Formatando valores com modificadores - Aula 05
+
+
+:s - Texto (strings)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:. (Número) f - Quantidade de casas decimais (float)
+:(Caracteres) (> ou < ^)(Quantidade (Tipo - s, d ou f)
+
+> - Esquerda
+< - Direita 
+^ - Centro
+
+"""
