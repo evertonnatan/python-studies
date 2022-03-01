2055ecf9392c74ed9ffec69f9b1c192bf7adaf95
evertonnatan <evertonnatandl@outlook.com>
2022-02-28 21:36:20 -0300

    Retomando os estudos de Python

---
+++ curso-python3/exercicios.py
@@ -0,0 +1,17 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro, informe se este número é par ou ímpar. Caso o usuário não digite um número inteiro, informe que não é um número inteiro. 
+
+"""
+
+
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário descrito, exiba a saudação apropriada. Ex.: Bom dia, Boa tarde, Boa noite!
+
+"""
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos, escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva "Seu nome é normal"; maior que 6 letras, escreva "Seu nome é muito grande".
+
+"""
+
