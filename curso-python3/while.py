5c676486ea1580153115670add1e47aaf4f82843
evertonnatan <evertonnatandl@outlook.com>
2022-03-05 08:24:31 -0300

    Continuar a aula dos últimos 5 minutos

--- curso-python3/while.py
+++ curso-python3/while.py
@@ -18,7 +18,37 @@ print("Não será executada")
 
 """
 
+"""
+# imprime na tela a sequência de 0 - 99
 x = 0
 while x < 100:
     print(x)
     x = x + 1
+
+"""
+
+# criando uma calculadora
+while True:
+    print()
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: ')
+    
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Você precisa digitar um número!')
+        continue
+
+        num_1 = int(num_1)
+        num_2 = int(num_2)
+
+    # Operadores: + - / * 
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    else:
+        print('Operador inválido')
