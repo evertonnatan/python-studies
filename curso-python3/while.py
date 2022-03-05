fce6bc5499699470f910197d6aed37b7cff0f962
evertonnatan <evertonnatandl@outlook.com>
2022-03-05 07:51:42 -0300

    Como funciona a estrutura "while" em Python

---
+++ curso-python3/while.py
@@ -0,0 +1,24 @@
+"""
+While em Python - Aula 07 => https://youtu.be/bkSePLdPiDk
+
+While é utilizado para realizar(repetir) ações enquanto uma condição for verdadeira.
+
+Requisitos: entender condições e operadores.
+
+"""
+
+from traceback import print_tb
+
+"""
+while True: # loop infinito
+    nome = input("Qual é o seu nome? ")
+    print(f'Olá {nome}! ')
+
+print("Não será executada")
+
+"""
+
+x = 0
+while x < 100:
+    print(x)
+    x = x + 1
