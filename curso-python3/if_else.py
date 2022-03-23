2c690579b01c5f7c2b67ec29cdd3cebc0626253d
evertonnatan <evertonnatandl@outlook.com>
2022-03-23 06:56:36 -0300

    Revisando condicionais em Python

--- curso-python3/if_else.py
+++ curso-python3/if_else.py
@@ -1,5 +1,14 @@
 '''
 Condições IF, ELIF e ELSE 
+
+A sintaxe de um comando if se parece com:
+
+    if EXPRESSÃO BOOLEANA:
+        COMANDOS_1        # executados se condição tem valor True
+    else:
+        COMANDOS_2        # executados se condição tem valor False
+
+Não há limite no número de comandos que podem aparecer nos dois blocos de um comando if, mas tem que haver pelo menos um comando em cada bloco. Cada bloco de else precisa ter exatamente um bloco de if correspondente. Se você quer encadear comandos if-else juntos, você precisa usar a construção de blocos aninhados. 
 '''
 
 if False:
